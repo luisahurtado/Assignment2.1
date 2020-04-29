21d4c2bb461ac359a258091ef4c4b5c3cd7d4f15
luisahurtado <61818251+luisahurtado@users.noreply.github.com>
2020-04-30 03:51:39 +1000

    Update q1a.sh

--- q1a.sh
+++ q1a.sh
@@ -1,5 +1,5 @@
-#!/bin/bash\cf3 \
-# Assignment 2 task 1  question 1.a.\cf3 \
-# Print the expression values that are \cf3 \
-# less than 1000 from the file expression.tab\cf3 \
-awk -F\'94\\t\'94 \'91$2 <= 1000' expression.tab}
+#!/bin/bash
+# Assignment 2 task 1  question 1.a.
+# Print the expression values that are
+# less than 1000 from the file expression.tab
+awk -F"\t" '$2 <= 1000' expression.tab
